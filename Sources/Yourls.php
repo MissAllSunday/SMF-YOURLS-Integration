d258e5ae6d09d9a28c9cb0fa44d359eb50c939e4
Suki <suki@missallsunday.com>
2013-05-03 10:16:52 -0500

    Class and language files
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/Yourls.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * SMF - YOURLS Integration
+ *
+ * @package SMF
+ * @author Suki <suki@missallsunday.com>
+ * @copyright 2013 Jessica González
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ *
+ * @version 1.0
+ */
+
+/*
+ * Version: MPL 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ */
+
+if (!defined('SMF'))
+	die('No direct access');
+
+class Yourls
+{
+	protected $name = 'Yourls';
+}
