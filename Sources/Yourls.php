ada6bddedb82f24d2de8b981b01e642b6740ee13
Suki <suki@missallsunday.com>
2013-05-03 10:26:25 -0500

    Some generic methods
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Yourls.php
+++ Sources/Yourls.php
@@ -31,4 +31,72 @@ if (!defined('SMF'))
 class Yourls
 {
 	protected $name = 'Yourls';
+
+	/**
+	 *
+	 *
+	 * @access public
+	 * @global string $sourcedir the path to the forum sources directory
+	 * @global array $smcFunc holds the SMF DB layer
+	 * @global array $modSettings global SMF settings
+	 * @return void
+	 */
+	public function __construct()
+	{
+		global $sourcedir, $smcFunc, $modSettings;
+
+		$this->_sourcedir = $sourcedir;
+		$this->_smcFunc = $smcFunc;
+		$this->_modSettings = $modSettings;
+	}
+
+	/**
+	 * Tries to fetch the content of a given url
+	 *
+	 * @access protected
+	 * @param string $url the url to call
+	 * @return mixed either the page requested or a boolean false
+	 */
+	protected function fetch_web_data($url)
+	{
+		/* Safety first! */
+		if (empty($url))
+			return false;
+
+		/* I can haz cURL? */
+		if (function_exists ('curl_init'))
+		{
+			/* From the remote API call sample */
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $api_url);
+			curl_setopt($ch, CURLOPT_HEADER, 0);            // No header in the result
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return, do not echo result
+			curl_setopt($ch, CURLOPT_POST, 1);              // This is a POST request
+			curl_setopt($ch, CURLOPT_POSTFIELDS, array(     // Data to POST
+					'url'      => $url,
+					'keyword'  => $keyword,
+					'format'   => $format,
+					'action'   => 'shorturl',
+					'username' => $username,
+					'password' => $password
+				));
+
+			/* Fetch */
+			$data = curl_exec($ch);
+			curl_close($ch);
+
+			/* Send the data directly, evil, I'm evil! :P */
+			return $data;
+		}
+
+		/* Good old SMF's fetch_web_data to the rescue! */
+		else
+		{
+			/* Requires a function in a source file far far away... */
+			require_once($this->_sourcedir .'/Subs-Package.php');
+
+			/* Send the result directly, we are gonna handle it on every case */
+			return fetch_web_data($url);
+		}
+	}
 }
