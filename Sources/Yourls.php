6549c01435e93dc04dc8a0adc82c8416f19d1c10
Suki <suki@missallsunday.com>
2013-05-05 10:24:46 -0500

    We don't need this no more
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Yourls.php
+++ Sources/Yourls.php
@@ -36,62 +36,6 @@ class Yourls
 	{
 	}
 
-	static function admin(&$areas)
-	{
-		global $txt;
-
-		if (!isset($txt['Yourls_title_main']))
-			loadLanguage(self::$name);
-
-		$areas['config']['areas']['modsettings']['subsections']['yourls'] = array($txt['Yourls_admin_title']);
-	}
-
-	static function settings(&$sub_actions)
-	{
-		global $context;
-
-		$sub_actions['yourls'] = 'Yourls::postSettings';
-		$context[$context['admin_menu_name']]['tab_data']['tabs']['yourls'] = array();
-	}
-
-	static function postSettings(&$return_config = false)
-	{
-		global $context, $scripturl, $txt;
-
-		$config_vars = array(
-			array('desc', self::$name .'_settingsDesc'),
-			array('check', self::$name .'_settingsEnable', 'subtext' => $txt[self::$name .'_settingsEnable_sub']),
-			array('text', self::$name .'_settingsDomain', 'subtext' => $txt[self::$name .'_settingsDomain_sub']),
-			array('text', self::$name .'_settingsUser', 'subtext' => $txt[self::$name .'_settingsUser_sub']),
-			array('text', self::$name .'_settingsPass', 'subtext' => $txt[self::$name .'_settingsPass_sub']),
-			array('check', self::$name .'_settingsEnableBBC', 'subtext' => $txt[self::$name .'_settingsEnableBBC_sub']),
-		);
-
-		if ($return_config)
-			return $config_vars;
-
-		$context['post_url'] = $scripturl . '?action=admin;area=modsettings;save;sa=yourls';
-		$context['settings_title'] = $txt[self::$name .'_admin_title'];
-
-		if (empty($config_vars))
-		{
-			$context['settings_save_dont_show'] = true;
-			$context['settings_message'] = '<div align="center">' . $txt['modification_no_misc_settings'] . '</div>';
-
-			return prepareDBSettingContext($config_vars);
-		}
-
-		if (isset($_GET['save']))
-		{
-			checkSession();
-			$save_vars = $config_vars;
-			saveDBSettings($save_vars);
-			redirectexit('action=admin;area=modsettings;sa=yourls');
-		}
-
-		prepareDBSettingContext($config_vars);
-	}
-
 	/**
 	 * Tries to fetch the content of a given url
 	 *
